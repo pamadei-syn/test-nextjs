39b5da94e32694e65d00553967e1ae407fa2457a
pamadei-syn <127850173+pamadei-syn@users.noreply.github.com>
2023-07-17 14:30:57 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h1>Hello there world</h1>
+      <h1>Hello there world 2</h1>
     </main>
   )
 }
